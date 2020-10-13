f6d4489a3452d97968d58b4b0d22a24c082d76c9
Alex Chi <iskyzh@gmail.com>
2020-10-13 10:42:12 +0800

    remove legacy code
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,11 +2,11 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("format make key with ts", |b| {
-        b.iter(|| agatedb::format::key_with_ts("aaabbbcccddd", 233))
+        b.iter(|| key_with_ts("aaabbbcccddd", 233))
     });
-    let key = agatedb::format::key_with_ts("aaabbbcccddd", 233);
+    let key = key_with_ts("aaabbbcccddd", 233);
     c.bench_function("format get ts", |b| {
-        b.iter(|| agatedb::format::get_ts(&key))
+        b.iter(|| get_ts(&key))
     });
     c.bench_function("format make key with ts (legacy)", |b| {
         b.iter(|| agatedb::format::legacy::key_with_ts("aaabbbcccddd", 233))
