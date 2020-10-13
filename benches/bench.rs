1351f744e05c7aad6d3345e2461cfe52e634cfd7
Alex Chi <iskyzh@gmail.com>
2020-10-13 14:13:36 +0800

    format code
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,3 +1,4 @@
+use agatedb::format::{get_ts, key_with_ts};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -5,16 +6,7 @@ fn criterion_benchmark(c: &mut Criterion) {
         b.iter(|| key_with_ts("aaabbbcccddd", 233))
     });
     let key = key_with_ts("aaabbbcccddd", 233);
-    c.bench_function("format get ts", |b| {
-        b.iter(|| get_ts(&key))
-    });
-    c.bench_function("format make key with ts (legacy)", |b| {
-        b.iter(|| agatedb::format::legacy::key_with_ts("aaabbbcccddd", 233))
-    });
-    let key = agatedb::format::legacy::key_with_ts("aaabbbcccddd", 233);
-    c.bench_function("format get ts (legacy)", |b| {
-        b.iter(|| agatedb::format::legacy::get_ts(&key))
-    });
+    c.bench_function("format get ts", |b| b.iter(|| get_ts(&key)));
 }
 
 criterion_group!(benches, criterion_benchmark);
