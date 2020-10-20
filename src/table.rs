830f8ce2ed38f2d561c920bac1a85ffcfad450db
Alex Chi <iskyzh@gmail.com>
2020-10-20 14:06:22 +0800

    table: add block iterator
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/table.rs
+++ src/table.rs
@@ -1,39 +1,59 @@
-pub mod builder;
-/*
-use std::fs;
-use bytes::Bytes;
-use proto::meta::{TableIndex, Checksum, Checksum_Algorithm, BlockOffset};
-use crate::opt::Options;
-use proto::meta::BlockOffset;
-use std::io::Error;
-use crate::Result;
-use crate::checksum;
-use std::path::Path;
+pub(crate) mod builder;
+mod iterator;
 
-struct File {
-    name: PathBuf,
-    file: fs::File,
+use crate::checksum;
+use crate::opt::Options;
+use crate::Error;
+use crate::Result;
+use bytes::{Buf, Bytes};
+use prost::Message;
+use proto::meta::{BlockOffset, Checksum, TableIndex};
+use std::fs;
+use std::io::{Read, Seek, Write};
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+use std::sync::Mutex;
+
+// TODO: use a mmap library instead of handling I/O on our own
+enum MmapFile {
+    File {
+        name: PathBuf,
+        // TODO: remove this mutex and allow multi-thread read
+        file: Mutex<fs::File>,
+    },
+    Memory {
+        data: Bytes,
+    },
+}
+
+impl MmapFile {
+    pub fn is_in_memory(&self) -> bool {
+        match self {
+            Self::File { .. } => false,
+            Self::Memory { .. } => true,
+        }
+    }
 }
 
 pub struct TableInner {
-    file: Option<File>,
+    file: MmapFile,
     table_size: usize,
     smallest: Bytes,
     biggest: Bytes,
     id: u64,
-    checksum: u64,
-    estimated_size: u64,
+    checksum: Bytes,
+    estimated_size: u32,
+    index: TableIndex,
     index_start: usize,
     index_len: usize,
-    is_in_memory: bool,
     opt: Options,
-    no_of_blocks: usize,
 }
 
 pub struct Table {
-    inner: Arc<Table>,
+    inner: Arc<TableInner>,
 }
 
+/*
 impl Drop for TableInner {
     fn drop(&mut self) {
         let f = match self.file.take() {
@@ -45,74 +65,14 @@ impl Drop for TableInner {
         fs::remove_file(&f.path).unwrap();
     }
 }
+*/
 
+#[derive(Default)]
 pub struct Block {
     offset: usize,
     data: Bytes,
-    checksum: Vec<u8>,
-    checksum: u64,
+    checksum: Bytes,
     entries_index_start: usize,
     entry_offsets: Vec<u32>,
     checksum_len: usize,
 }
-
-impl Block {
-    fn size(&self) -> u64 {
-        3 * mem::size_of::<usize>() as u64 + self.data.len() as u64 + self.checksum.len() as u64 + self.entry_offsets.len() as u64 * mem::size_of::<u32>() as u64
-    }
-
-    fn verify_checksum(&self) -> Result<()> {
-        let mut chksum = CheckSum::default();
-        chksum.merge_from_bytes(&self.checksum)?;
-        checksum::verify_checksum(&self.data, &chksum)
-    }
-}
-
-fn parse_file_id(name: &str) -> (u64, bool) {
-    if !name.ends_with(".sst") {
-        (0, false)
-    }
-    match name[..name.len() - 4].parse() {
-        Ok(id) => (id, true),
-        Err(_) => return (0, false),
-    }
-}
-
-impl Table {
-    pub fn open(path: &Path, opt: Options) -> Result<Table> {
-        let f = fs::File::open(path)?;
-        let file_name = path.base_name();
-        let (id, ok) = parse_file_id(file_name);
-        if !ok {
-            return Err();
-        }
-        let meta = f.metadata()?;
-        let table_size = meta.len();
-        let mut t = Table {
-            inner: Arc::new(Table {
-                file: Some(File {
-                    name: path.to_buf(),
-                    file: f,
-                }),
-                table_size,
-                smallest: Bytes::new(),
-                biggest: Bytes::new(),
-                id,
-                checksum: 0,
-                estimated_size: 0,
-                index_start: 0,
-                index_len: 0,
-                is_in_memory: false,
-                opt,
-                no_of_blocks: 0,
-            }),
-        };
-
-    }
-
-    fn init_biggest_and_smallest(&mut self) -> Result<()> {
-        self.read_index()?;
-
-    }
-}
-*/
---
+++ src/table/iterator.rs
@@ -0,0 +1,166 @@
+use super::builder::{Header, HEADER_SIZE};
+use super::{Block, TableInner};
+use crate::util::{self, KeyComparator, COMPARATOR};
+use crate::value::Value;
+use bytes::Bytes;
+use std::sync::Arc;
+
+#[derive(Clone, Debug)]
+pub enum IteratorError {
+    NoError,
+    EOF,
+    // TODO: As we need to clone Error from block iterator to table iterator,
+    // we had to save `crate::Error` as String. In the future, we could let all
+    // seek-related function to return a `Result<()>` instead of saving an
+    // error inside struct.
+    Error(String),
+}
+
+impl IteratorError {
+    pub fn is_err(&self) -> bool {
+        match self {
+            IteratorError::NoError => false,
+            _ => true,
+        }
+    }
+
+    pub fn is_eof(&self) -> bool {
+        match self {
+            IteratorError::EOF => true,
+            _ => false,
+        }
+    }
+}
+
+enum SeekPos {
+    Origin,
+    Current,
+}
+
+
+/// Block iterator iterates on an SST block
+// TODO: support custom comparator
+struct BlockIterator {
+    idx: isize,
+    base_key: Bytes,
+    key: Bytes,
+    val: Bytes,
+    data: Bytes,
+    // TODO: use `&'a Block` if possible
+    block: Arc<Block>,
+    perv_overlap: u16,
+    err: IteratorError,
+}
+
+impl BlockIterator {
+    pub fn new(block: Arc<Block>) -> Self {
+        let data = block.data.slice(..block.entries_index_start);
+        Self {
+            block,
+            err: IteratorError::NoError,
+            base_key: Bytes::new(),
+            key: Bytes::new(),
+            val: Bytes::new(),
+            data,
+            perv_overlap: 0,
+            idx: 0,
+        }
+    }
+
+    #[inline]
+    fn entry_offsets(&self) -> &[u32] {
+        &self.block.entry_offsets
+    }
+
+    fn set_idx(&mut self, i: isize) {
+        self.idx = i;
+        if i >= self.entry_offsets().len() as isize || i < 0 {
+            self.err = IteratorError::EOF;
+            return;
+        }
+
+        self.err = IteratorError::NoError;
+
+        // if base key is not available, retrieve it from first key-value pair in block
+        if self.base_key.is_empty() {
+            let mut base_header = Header::default();
+            base_header.decode(&self.data);
+            // TODO: combine this decode with header decode to avoid slice ptr copy
+            self.base_key = self
+                .data
+                .slice(HEADER_SIZE..HEADER_SIZE + base_header.diff as usize);
+        }
+
+        let start_offset = self.entry_offsets()[i as usize] as u32;
+        let end_offset = if self.idx + 1 == self.entry_offsets().len() as isize {
+            self.data.len()
+        } else {
+            self.entry_offsets()[self.idx as usize + 1] as usize
+        };
+
+        let mut entry_data = self.data.slice(start_offset as usize..end_offset as usize);
+        let mut header = Header::default();
+        header.decode(&mut entry_data);
+
+        // TODO: optimize base_key copy
+        // if header.overlap > self.perv_overlap {
+        //     self.key = Bytes::from(
+        //         [
+        //             &self.key[..self.perv_overlap as usize],
+        //             &self.base_key[self.perv_overlap as usize..header.overlap as usize],
+        //         ]
+        //         .concat(),
+        //     );
+        // }
+        // self.perv_overlap = header.overlap;
+
+        let diff_key = &entry_data[..header.diff as usize];
+        self.key = Bytes::from([&self.base_key[..header.overlap as usize], diff_key].concat());
+        self.val = entry_data.slice(header.diff as usize..);
+    }
+
+    pub fn valid(&self) -> bool {
+        !self.err.is_err()
+    }
+
+    pub fn error(&self) -> &IteratorError {
+        &self.err
+    }
+
+    pub fn seek(&mut self, key: &Bytes, whence: SeekPos) {
+        self.err = IteratorError::NoError;
+        let start_index = match whence {
+            SeekPos::Origin => 0,
+            SeekPos::Current => self.idx,
+        };
+        let found_entry_idx = util::search(self.entry_offsets().len(), |idx| {
+            use std::cmp::Ordering::*;
+            if idx < start_index as usize {
+                return false;
+            }
+            self.set_idx(idx as isize);
+            match COMPARATOR.compare_key(&self.key, &key) {
+                Less => false,
+                _ => true,
+            }
+        });
+
+        self.set_idx(found_entry_idx as isize);
+    }
+
+    pub fn seek_to_first(&mut self) {
+        self.set_idx(0);
+    }
+
+    pub fn seek_to_last(&mut self) {
+        self.set_idx(self.entry_offsets().len() as isize - 1);
+    }
+
+    pub fn next(&mut self) {
+        self.set_idx(self.idx + 1);
+    }
+
+    pub fn prev(&mut self) {
+        self.set_idx(self.idx - 1);
+    }
+}
