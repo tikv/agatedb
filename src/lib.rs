828e0a5378aa0ead8f378197be7aad96e0b0fd6e
Alex Chi <iskyzh@gmail.com>
2020-11-10 10:15:47 +0800

    format
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,8 @@ mod value;
 mod wal;
 
 pub use format::{get_ts, key_with_ts};
-pub use opt::Options as TableOptions;
 pub use opt::ChecksumVerificationMode;
+pub use opt::Options as TableOptions;
 pub use table::builder::Builder as TableBuilder;
 pub use table::Table;
 pub use value::Value;
