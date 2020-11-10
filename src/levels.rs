6edceac9a50a7824662f91321e85526db057e4a2
Alex Chi <iskyzh@gmail.com>
2020-11-10 11:48:23 +0800

    add fields
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/levels.rs
+++ src/levels.rs
@@ -1,8 +1,37 @@
-use crate::Table;
+use crate::{Table, AgateOptions};
+use crate::db::Core as AgateCore;
+use std::sync::atomic::AtomicU64;
+use crate::Result;
 
-pub struct LevelController {}
+struct CompactStatus {
 
-impl LevelController {
+}
+struct LevelHandler {
+
+}
+
+pub struct LevelsController {
+    next_file_id: AtomicU64,
+
+    levels: Vec<LevelHandler>,
+    opts: AgateOptions,
+    // TODO: agate oracle, manifest should be added here
+
+    cpt_status: CompactStatus
+}
+
+impl LevelsController {
+    pub fn new(db: &AgateCore) -> Result<Self> {
+        let lvctl = Self {
+            next_file_id: AtomicU64::new(0),
+            levels: vec![],
+            opts: db.opts.clone(),
+            cpt_status: CompactStatus {}
+        };
+
+        Ok(lvctl)
+    }
+
     pub fn reserve_file_id(&self) -> u64 {
         0
     }
