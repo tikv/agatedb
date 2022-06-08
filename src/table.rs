c5d111ac638f0032cdc0e57ce124412a9ad39ad8
GanZiheng <ganziheng98@gmail.com>
2022-06-08 20:47:19 +0800

    update
    Signed-off-by: GanZiheng <ganziheng98@gmail.com>

--- src/table.rs
+++ src/table.rs
@@ -213,30 +213,22 @@ impl TableInner {
         let mut read_pos = self.table_size;
 
         // read checksum length from last 4 bytes
-        read_pos = read_pos
-            .checked_sub(4)
-            .ok_or_else(|| Error::TableRead("Size of table less than 4.".into()))?;
+        read_pos -= 4;
         let mut buf = self.read(read_pos, 4)?;
         let checksum_len = buf.get_u32() as usize;
 
         // read checksum
-        read_pos = read_pos
-            .checked_sub(checksum_len)
-            .ok_or_else(|| Error::TableRead("Checksum length overflow.".into()))?;
+        read_pos -= checksum_len;
         let buf = self.read(read_pos, checksum_len)?;
         let chksum = Checksum::decode(buf)?;
 
         // read index size from footer
-        read_pos = read_pos
-            .checked_sub(4)
-            .ok_or_else(|| Error::TableRead("Cannot read length of index.".into()))?;
+        read_pos -= 4;
         let mut buf = self.read(read_pos, 4)?;
         self.index_len = buf.get_u32() as usize;
 
         // read index
-        read_pos = read_pos
-            .checked_sub(self.index_len)
-            .ok_or_else(|| Error::TableRead("Index length overflow.".into()))?;
+        read_pos -= self.index_len;
         self.index_start = read_pos;
         let data = self.read(read_pos, self.index_len)?;
         checksum::verify_checksum(&data, &chksum)?;
