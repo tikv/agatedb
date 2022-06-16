1b457113b4414e26b8e458997dba60c39c0ee1ca
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2022-06-16 11:06:08 +0800

    db: start bg compaction threads after init agate (#148)
    * agate: start bg compaction threads after init agate
    
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>
    
    Co-authored-by: GanZiheng <ganziheng98@gmail.com>

--- src/db.rs
+++ src/db.rs
@@ -5,6 +5,7 @@ use std::{
     fs,
     path::{Path, PathBuf},
     sync::{atomic::AtomicUsize, Arc, RwLock},
+    thread::JoinHandle,
 };
 
 use bytes::Bytes;
@@ -12,7 +13,6 @@ use crossbeam_channel::{Receiver, Sender};
 use log::debug;
 pub use opt::AgateOptions;
 use skiplist::Skiplist;
-use yatp::task::callback::Handle;
 
 use super::{
     manifest::ManifestFile,
@@ -21,6 +21,7 @@ use super::{
     Error, Result,
 };
 use crate::{
+    closer::Closer,
     format::get_ts,
     levels::LevelsController,
     ops::oracle::Oracle,
@@ -44,7 +45,12 @@ pub struct Core {
 
 pub struct Agate {
     pub(crate) core: Arc<Core>,
+    closer: Closer,
     pool: Arc<yatp::ThreadPool<yatp::task::callback::TaskCell>>,
+
+    // TODO: replace flush thread with yatp pool, so we can control the total
+    // cpu usage by control the unified yatp pool.
+    flush_handle: Option<JoinHandle<()>>,
 }
 
 struct FlushTask {
@@ -75,27 +81,31 @@ impl Agate {
 
     fn new(core: Arc<Core>) -> Self {
         let flush_core = core.clone();
+        let closer = Closer::new();
+        let pool = Arc::new(yatp::Builder::new("agatedb").build_callback_pool());
 
-        let agate = Self {
+        let flush_handle = std::thread::spawn(move || flush_core.flush_memtable().unwrap());
+
+        core.lvctl.start_compact(closer.clone(), pool.clone());
+
+        Self {
             core,
-            pool: Arc::new(yatp::Builder::new("agatedb").build_callback_pool()),
-        };
-
-        agate
-            .pool
-            .spawn(move |_: &mut Handle<'_>| flush_core.flush_memtable().unwrap());
-
-        agate
-    }
-
-    fn close(&self) {
-        self.core.flush_channel.0.send(None).unwrap();
+            closer,
+            pool,
+            flush_handle: Some(flush_handle),
+        }
     }
 }
 
 impl Drop for Agate {
     fn drop(&mut self) {
-        self.close();
+        // Flush thread need to be closed before compaction threads. Because the flush
+        // thread may stuck in add_l0_table forever when there are too many sst files
+        // in level0, and at the same time all compaction threads have exited.
+        // TODO: remove such closing order dependency
+        self.core.flush_channel.0.send(None).unwrap();
+        self.flush_handle.take().unwrap().join().unwrap();
+        self.closer.close();
         self.pool.shutdown();
     }
 }
